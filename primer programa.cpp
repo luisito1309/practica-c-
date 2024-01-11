6933ec430c4dbeea0d3b029af7307d736df18ee9
joseandres2003 <155786378+joseandres2003@users.noreply.github.com>
2024-01-10 21:20:01 -0400

    Add files via upload

---
+++ primer programa.cpp
@@ -0,0 +1,21 @@
+/*
+titulo: primier programa con comentario
+autor: Juan David Meza Gonzles
+descripcion: primer programa para analizar la estructura.
+fecha: 2024-01-10
+*/
+#include <iostream> //inclusion de la libreria de entrada y salidas.
+using namespace std; //el uso de espacio de nombre para STD de la I/O
+int main() // Función principal para retornar un entero.
+{ 
+	int numero; // Declaración de la variable numero como entero.
+	
+	numero = 5 + 5; // Asignación de la suma de 2 números (5 + 5) a la variable numero.
+	/*
+	impresion de resultado de la variable de numero
+	y adicion al final del salto de linea.
+	*/
+	cout << "El resultado es: " << numero << endl; // Esta línea muestra el mensaje "El resultado es:" seguido del valor de la variable numero.y un salto de linea.
+	return 0; // Retorno de 0 para finalizar el programa.
+}
+
